d57e1062cb32c368e1229f7a2cdad9b5ce7aa2fd
Michael Agbiaowei <bryanwalton1997@gmail.com>
2023-03-28 12:22:49 +0000

    updated

--- noted/core/settings/base.py
+++ noted/core/settings/base.py
@@ -46,7 +46,7 @@ TEST_MODE = False
 
 SITE_ID = 1
 
-ALLOWED_HOSTS = ["https://8000-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 ADMINS = [(get_env_variable("ADMIN_NAME"), get_env_variable("ADMIN_EMAIL"))]
 
--- noted/core/settings/local.py
+++ noted/core/settings/local.py
@@ -3,7 +3,7 @@
 from .base import *
 
 
-ALLOWED_HOSTS += ["127.0.0.1", "localhost", "34.235.157.10"]
+ALLOWED_HOSTS += ["127.0.0.1", "localhost"]
 
 INTERNAL_IPS = ["127.0.0.1"]
 
