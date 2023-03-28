adc7d8df290081c40b8e110b35be89496dcf135a
Michael Agbiaowei <bryanwalton1997@gmail.com>
2023-03-28 19:20:50 +0000

    updated

--- noted/core/settings/base.py
+++ noted/core/settings/base.py
@@ -46,7 +46,7 @@ TEST_MODE = False
 
 SITE_ID = 1
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://8000-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}"]
 
 ADMINS = [(get_env_variable("ADMIN_NAME"), get_env_variable("ADMIN_EMAIL"))]
 
